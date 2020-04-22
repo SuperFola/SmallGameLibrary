38e901bd21ef99c765c142fb689d7bb539ce2b56
SuperFola <lexplt.dev@gmail.com>
2020-04-22 16:00:54 +0200

    adding 2 new widgets, still need to work on them

---
+++ include/Small/Widgets/CheckBox.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file CheckBox.hpp
+ * @author Alexandre Plateau (lexplt.dev@gmail.com)
+ * @brief Basic checkbox widget
+ * @version 0.1
+ * @date 2020-04-18
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef sgl_small_widgets_checkbox
+#define sgl_small_widgets_checkbox
+
+#include <Small/Widgets/Base.hpp>
+
+namespace sgl::Widgets
+{
+    // TODO
+}
+
+#endif
---
+++ include/Small/Widgets/TextBox.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file TextBox.hpp
+ * @author Alexandre Plateau (lexplt.dev@gmail.com)
+ * @brief Basic textbox widget
+ * @version 0.1
+ * @date 2020-04-18
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef sgl_small_widgets_textbox
+#define sgl_small_widgets_textbox
+
+#include <Small/Widgets/Base.hpp>
+
+namespace sgl::Widgets
+{
+    // TODO
+}
+
+#endif
